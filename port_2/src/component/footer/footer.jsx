b66ae84ceb27d3ba1881e6dabc826c8268c7bde1
sangeetha <Artemis6161@github.com>
2022-06-15 13:12:43 +0530

    foo

--- port_2/src/component/footer/footer.jsx
+++ port_2/src/component/footer/footer.jsx
@@ -37,7 +37,7 @@ const footer = () => {
 </div>
 
 
-    <div className="btn">
+    {/* <div className="btn">
               
 
               <Nav.Item className="fork-btn">
@@ -54,11 +54,93 @@ const footer = () => {
               </Nav.Link>
             </Nav.Item>
         
-            </div>
+            </div> */}
            
   </div>
+
   
 );
 };
 
 export default footer
+
+
+// import Button from 'react-bootstrap/Button';
+//  import './footer.css'
+// import Card from 'react-bootstrap/Card';
+// import Nav from "react-bootstrap/Nav";
+// import { CgGitFork } from "react-icons/cg";
+// import {
+ 
+//     FaInstagram,
+//     FaLinkedin,
+//     FaGithub,
+   
+//   } from "react-icons/fa";
+//   import {  FiGithub,FiLinkedin } from "react-icons/fi"
+// import {
+//   AiFillStar,
+
+// } from "react-icons/ai";
+// function HeaderAndFooterExample() {
+//   return (
+//     <Card className="text-center">
+//       {/* <Card.Header>Featured</Card.Header> */}
+//       <Card.Body>
+//       <Nav.Item className="btn">
+//       <Nav.Link
+//              href="https://www.linkedin.com/in/sangeetha-rj-283106198/"
+//                target="_blank"
+//                rel="noreferrer"
+//                  className="fork-btn-inner">
+
+// <FaLinkedin   size="3rem"/>
+                      
+               
+//                </Nav.Link>
+//                <Nav.Link
+//              href="https://github.com/Artemis6161"
+//                target="_blank"
+//                rel="noreferrer"
+//                  className="fork-btn-inner">
+
+// <FiGithub size="3rem"/>
+                      
+               
+//                </Nav.Link>
+//                <Nav.Link
+//              href="https://www.instagram.com/spiceitupwithsan61/"
+//                target="_blank"
+//                rel="noreferrer"
+//                  className="fork-btn-inner">
+// < FaInstagram size="3rem"/>
+                      
+               
+//                </Nav.Link>
+             
+//                </Nav.Item>
+
+
+//         <Card.Text>
+//         Designed & Built by Sangeetha Rajan.
+//         </Card.Text>
+//         <Nav.Item className="fork-btn">
+//              <Nav.Link
+//              href="https://github.com/Artemis6161/portfoilo_2.0/tree/master/port_2"
+//                target="_blank"
+//                rel="noreferrer"
+//                  className="fork-btn-inner">
+
+
+//                         <CgGitFork style={{ fontSize: "1.2em" }} size="30px" color= '#242D49'/>{" "}
+//                 <AiFillStar style={{ fontSize: "1.1em" }} size="30px"color= '#242D49' />
+               
+//                </Nav.Link>
+//             </Nav.Item>
+//       </Card.Body>
+//       {/* <Card.Footer className="text-muted">2 days ago</Card.Footer> */}
+//     </Card>
+//   );
+// }
+
+// export default HeaderAndFooterExample;
